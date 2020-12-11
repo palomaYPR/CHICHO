27b0d1c882a66c0aeeab87333524ee75e5cbfb0c
Paloma Pérez <40873358+palomaYPR@users.noreply.github.com>
2020-12-11 14:17:28 -0600

    Update registroemail.py

--- registroemail.py
+++ registroemail.py
@@ -4,23 +4,23 @@ from datetime import datetime
 def search(nombre,temp):
     name = nombre
     fech = datetime.now()
-    db = pymysql.connect(host='localhost', user='root', passwd='sistemas', db='delfin2020')
+    db = pymysql.connect(host='localhost', user='root', passwd='', db='')
     cursor = db.cursor()
     id = cursor.execute("SELECT id_empleado FROM empleados WHERE nombre = %s;", (name,))
     cursor.execute("SELECT * FROM chicho WHERE id_empleados = %s;", (id,))
     if cursor.fetchone():
-        # Significa que es salida
+        # checkout
         update_exit(id,fech)
         cursor.close()
         return 0
     else:
-        #Significa que es entrada
+        # checkin
         #Boolean Temp
         insert_information(id,fech,temp)
         return 1
 
 def update_exit(id,fech):
-    db = pymysql.connect(host='localhost', user='root', passwd='sistemas', db='delfin2020')
+    db = pymysql.connect(host='localhost', user='root', passwd='', db='')
     cursor = db.cursor()
     fechaE = cursor.execute("SELECT HoraE FROM chicho WHERE id_empleado = %s;", (id,))
     fE = fechaE.split(' ')[0]
@@ -32,7 +32,7 @@ def update_exit(id,fech):
 
 
 def insert_information(id,date,temp):
-    db = pymysql.connect(host='localhost', user='root', passwd='sistemas', db='delfin2020')
+    db = pymysql.connect(host='localhost', user='root', passwd='', db='')
     cursor = db.cursor()
     cursor.execute("INSERT INTO chicho (id, id_empleado, HoraE, Temp) VALUES (%s, %s, %s, %s)", (0, id, date, temp))
     cursor.close()
